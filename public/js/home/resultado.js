3a85536fe466efd891c6dd27ef9a81bae5128999
Sebastian <spedgra@alu.upo.es>
2020-01-26 01:53:46 +0100

    Añadidas pantallas, funcionalidades y arreglado error  url imagenes
    Pantallas añadidas:
    - Modificacion cuenta
    - CRUD de inmueble (añadida a espacio usuario y arreglada)
    - Plantilla de backend
    
    Funcionalidades añadidas:
    - Obtencion de datos de inmueble por AJAX (resultado.js)
    - Añadido setCenter en funcion de localizacion del inmueble en maps API (resultado.js)
    - Añadida marca en el mapa en maps API (resultado.js)
    - Añadido popup en el marker (resultado.js)

--- public/js/home/resultado.js
+++ public/js/home/resultado.js
@@ -1,11 +1,48 @@
 $(document).ready(function(){
-    initMap();
+  ajaxGetLocaclizacion();
 });
 
-function initMap() {
+function initMap(longitud, latitud, direccion) {
+    var latLng = new google.maps.LatLng(latitud, longitud);
     var map = new google.maps.Map(document.getElementById('map'), {
-      center: new google.maps.LatLng(-33.863276, 151.207977),
-      zoom: 12
+      center: latLng,
+      zoom: 20
     });
+
+    var infowindow = new google.maps.InfoWindow({
+      content: direccion
+    });
+  
+    
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+      title: direccion
+    });
+
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+  
+
+    marker.setMap(map);
+
+  
     var infoWindow = new google.maps.InfoWindow;
+    
 }
+
+function ajaxGetLocaclizacion(){
+var idInmueble = parseInt($("#idInmueble").val());
+    
+    var localizacion;
+    $.ajax({
+      method: "POST",
+      url: "../ajax/ajaxGetLocalizacion/"+idInmueble,
+      dataType: 'json',
+      data: {"idInmueble": idInmueble},
+      success: function (result) {
+        initMap(result.longitud, result.latitud, result.direccion);
+      }
+    });
+  }
--- src/Controller/InmuebleController.php
+++ src/Controller/InmuebleController.php
@@ -16,12 +16,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class InmuebleController extends AbstractController
 {
     /**
-     * @Route("/", name="inmueble_index", methods={"GET"})
+     * @Route("/index/{id}", name="inmueble_index", methods={"GET"})
      */
-    public function index(InmuebleRepository $inmuebleRepository): Response
+    public function index(int $id, InmuebleRepository $inmuebleRepository): Response
     {
         return $this->render('inmueble/index.html.twig', [
-            'inmuebles' => $inmuebleRepository->findAll(),
+            'inmuebles' => $inmuebleRepository->findBy(array("idCreador"=>$id)),
         ]);
     }
 
@@ -30,16 +30,31 @@ class InmuebleController extends AbstractController
      */
     public function new(Request $request): Response
     {
+        $session = $request->getSession();
+
         $inmueble = new Inmueble();
         $form = $this->createForm(InmuebleType::class, $inmueble);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $direccion = $_POST["inmueble"]["direccion"].", ".$_POST["inmueble"]["ciudad"]." ".$_POST["inmueble"]["cp"];
+            $geo = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($direccion).'&key=AIzaSyBX1Qy2dFMigK3r7pwgCBFC90exmctPt6g ');
+            $geo = json_decode($geo, true);
+
+
+            $latitud = $geo['results'][0]['geometry']['location']['lat'];
+            $longitud = $geo['results'][0]['geometry']['location']['lng'];
+            
+            $inmueble->setLatitud($latitud);
+            $inmueble->setLongitud($longitud);
+
+            $inmueble->setIdCreador($session->get("usuario_id"));
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($inmueble);
             $entityManager->flush();
 
-            return $this->redirectToRoute('inmueble_index');
+            return $this->redirectToRoute('inmueble_index',array("id"=>$session->get("usuario_id")));
         }
 
         return $this->render('inmueble/new.html.twig', [
@@ -49,12 +64,12 @@ class InmuebleController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="inmueble_show", methods={"GET"})
+     * @Route("show/{id}", name="inmueble_show", methods={"GET"})
      */
-    public function show(Inmueble $inmueble): Response
+    public function show(int $id, InmuebleRepository $inmuebleRepository): Response
     {
         return $this->render('inmueble/show.html.twig', [
-            'inmueble' => $inmueble,
+            'inmueble' => $inmuebleRepository->find($id),
         ]);
     }
 
@@ -63,13 +78,27 @@ class InmuebleController extends AbstractController
      */
     public function edit(Request $request, Inmueble $inmueble): Response
     {
+        $session = $request->getSession();
+
         $form = $this->createForm(InmuebleType::class, $inmueble);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $direccion = $_POST["inmueble"]["direccion"].", ".$_POST["inmueble"]["ciudad"]." ".$_POST["inmueble"]["cp"];
+            $geo = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($direccion).'&key=AIzaSyBX1Qy2dFMigK3r7pwgCBFC90exmctPt6g ');
+            $geo = json_decode($geo, true);
+
+
+            $latitud = $geo['results'][0]['geometry']['location']['lat'];
+            $longitud = $geo['results'][0]['geometry']['location']['lng'];
+                
+            $inmueble->setLatitud($latitud);
+            $inmueble->setLongitud($longitud);
+
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('inmueble_index');
+            return $this->redirectToRoute('inmueble_index',array("id"=>$session->get("usuario_id")));
         }
 
         return $this->render('inmueble/edit.html.twig', [
@@ -83,12 +112,15 @@ class InmuebleController extends AbstractController
      */
     public function delete(Request $request, Inmueble $inmueble): Response
     {
+        $session = $request->getSession();
         if ($this->isCsrfTokenValid('delete'.$inmueble->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($inmueble);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('inmueble_index');
+        return $this->redirectToRoute('inmueble_index',array("id"=>$session->get("usuario_id")));
     }
+
+    
 }
