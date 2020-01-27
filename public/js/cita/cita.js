3cdf5e1394791682c87ae86f1656d0f7ea8a7dd0
Sebastian <spedgra@alu.upo.es>
2020-01-27 19:46:36 +0100

    Calendario cita funciona

--- public/js/cita/cita.js
+++ public/js/cita/cita.js
@@ -4,13 +4,37 @@ $(document).ready(function(){
         url: "../ajax/ajaxGetCitas",
         dataType: 'json',
         success: function (result) {
-            console.log(result);
-            var eventos;
+
+            var eventos = new Array();
+            
+            result.forEach(element => {
+                var evento = new Object();
+                var fechaHora = element.fechaHora.date.split(" ");
+
+                evento.id=element.id;
+                evento.name=element.direccion+" "+element.ciudad;
+                evento.startdate=fechaHora[0];
+                evento.starttime=fechaHora[1];
+                evento.color="#FFB128";
+                evento.url="";
+
+                eventos.push(evento);
+            });
+
+            var eventos_calendario = new Object();
+
+            eventos_calendario.monthly=eventos;
+
+            var eventos_json = JSON.stringify(eventos_calendario);
+            eventos_json = JSON.parse(eventos_json);
+
+            console.log(eventos_json);
+            
             $('#mycalendar').monthly({
                 mode: 'event',
                 stylePast: true,
                 dataType: 'json',
-                events: eventos,
+                events: eventos_json,
                 weekStart: 'Mon'
             });
         }
