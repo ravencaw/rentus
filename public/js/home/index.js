c09f23063931d3f116b7a9b2d3b8939b53c37871
Sebastian <spedgra@alu.upo.es>
2020-01-28 13:37:49 +0100

    Busqueda sin arreglar (origen)

--- public/js/home/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".submit").on("click", function(){
-        $ciudad = $(".ciudad").val();
-        $tipo = $(".tipo").val();
-        $("#form_busqueda_home").attr("action", "home/busqueda/"+ciudad+"/"+tipo)
-    });
-});
